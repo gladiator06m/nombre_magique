937500e73ef73a1082fed022dc301ce9e820c389
gladiator06m <n.khedaiji@gmail.com>
2021-03-03 13:19:24 +0100

    projet premier commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace nombre_magique
+{
+    class Program
+    {
+        //FONCTION DEMANDEZ NOMBRE
+
+        static int demanderNombre()
+        {
+            
+           int chiffre = 0;
+         
+
+            while (chiffre <= 0)
+            {
+                Console.Write("Veuillez rentrer un nombre entre 1 et 10 : ");
+                string reponse = Console.ReadLine();
+
+            
+                try
+                {
+                    chiffre = int.Parse(reponse);
+                    Console.WriteLine("Vous avez rentré le nombre  " + chiffre);
+                }
+                catch
+                {
+
+                    Console.WriteLine("Erreur vous devez rentrer un nombre valide");  
+                
+                }
+               
+            }
+            return chiffre;
+        }
+
+        static void Main(string[] args)
+        {
+            demanderNombre();
+
+        }
+    }
+}
