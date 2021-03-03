cc55e92afa0c8c527d93d93e9530673f87b7f205
gladiator06m <n.khedaiji@gmail.com>
2021-03-03 19:10:44 +0100

    fin de journée

--- Program.cs
+++ Program.cs
@@ -6,37 +6,44 @@ namespace nombre_magique
     {
         //FONCTION DEMANDEZ NOMBRE
 
-        static int demanderNombre()
+        static int demanderNombre(int min, int max)
         {
             
            int chiffre = 0;
          
 
-            while (chiffre <= 0)
+            while ((chiffre < min) || (chiffre > max))
             {
-                Console.Write("Veuillez rentrer un nombre entre 1 et 10 : ");
+                Console.Write("Veuillez rentrer un nombre entre " + min + " et " + max + " : " );
                 string reponse = Console.ReadLine();
-
             
                 try
                 {
                     chiffre = int.Parse(reponse);
                     Console.WriteLine("Vous avez rentré le nombre  " + chiffre);
+
+                    if (chiffre > max)
+                    {
+                        Console.WriteLine("Erreur vous devez rentrer un nombre de 1 à 10");
+                    }
                 }
+
                 catch
                 {
-
                     Console.WriteLine("Erreur vous devez rentrer un nombre valide");  
-                
                 }
-               
+
             }
             return chiffre;
         }
 
         static void Main(string[] args)
         {
-            demanderNombre();
+
+            const int nombre_min = 1;
+            const int nombre_max = 10;
+
+            demanderNombre( nombre_min, nombre_max);
 
         }
     }
